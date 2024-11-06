3ff2dd0323ad51f0f6c1f5c06527c7645d123de9
aleks1303 <aleksbulda13@gmail.com>
2024-11-06 17:57:59 +0200

    react_preview_forms_use-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import FormComponents from "./components/FormComponents";
 
 function App() {
   return (
     <div>
-
+        <FormComponents/>
     </div>
   );
 }
---
+++ src/components/FormComponents.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {useForm} from "react-hook-form";
+
+interface IFormProps {
+    username:string,
+    password:string,
+    age:number
+}
+
+const FormComponents = () => {
+    
+    const {
+        handleSubmit,register} = useForm<IFormProps>();
+    const customerHandler = (formDataProps:IFormProps)=>{
+        console.log(formDataProps)
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit(customerHandler)}>
+                <input type="text" {...register('username')}/>
+                <input type="text" {...register('password')} />
+                <input type="number" {...register('age')} />
+                <button>send</button>
+            </form>
+        </div>
+    );
+};
+
+export default FormComponents;
