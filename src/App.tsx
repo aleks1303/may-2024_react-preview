93bd55ab5089ff4eb34f4a472882b8a6c62322a1
aleks1303 <aleksbulda13@gmail.com>
2024-11-12 20:00:51 +0200

    react_preview_useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {FC, useState} from 'react';
 import './App.css';
 
-function App() {
-  // що робить useState
-  // useState - це спеціальний hook, який дозволяє при зміні стану якогось об'єкту:
-  // (числа, user, масиву) запустити процес переріндинінгу,
-  // якщо так не працювати ніякий рірендерів не буде відбуватись
-  // тобто при натисанні на кнопу
+// що робить useState
+// useState - це спеціальний hook, який дозволяє при зміні стану якогось об'єкту:
+// (числа, user, масиву) запустити процес переріндинінгу,
+// якщо так не працювати ніякий рірендерів не буде відбуватись
+// тобто при натисанні на кнопку в розмітці нічого не буде змінюватись
+const App:FC = () => {
+  let [number, setNumber] = useState<number>(0);
+  const increment = () =>{
+    setNumber(prevState => prevState + 1)
+  }
+
   return (
     <div>
-
+        <h2>{number}</h2>
+        <button onClick={increment}>increment</button>
     </div>
   );
 }
