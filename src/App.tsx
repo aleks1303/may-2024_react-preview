ecfcf2e66dfc6b0ff27dab43ad0bab4b030c0397
aleks1303 <aleksbulda13@gmail.com>
2024-11-12 23:05:43 +0200

    react_preview_custom-hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {FC} from 'react';
 import './App.css';
+import {useFetch} from "./hooks/CustomHooks";
+import {IUser} from "./models/IUser";
 
-function App() {
-  return (
-    <div>
 
-    </div>
+// можна створити свій власний hook
+// власний hook відрізняється від бібліотечного тим,
+// що в середині custom hook можна використовувати бібліотечні hooks
+// або будь-які інші
+// в середині звичайної функції цього зробити не можна
+// hook від function відрізняється тим, що починається зі слова use
+// якщо function починається на слово use - це custom hook
+
+
+const App :FC = () => {
+
+  const users = useFetch<IUser[]>('/users', []);
+  return  (
+    <>
+      {
+      users.map(user => (<div>{user.name}</div>))
+      }
+    </>
   );
 }
 
---
+++ src/hooks/CustomHooks.tsx
@@ -0,0 +1,18 @@
+// створимо власний hook
+
+import {useEffect, useState} from "react";
+
+const useFetch = <T,> (endpoint:string,init:T) =>{
+    const [data, setData] = useState(init)
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com' + endpoint)
+            .then(value => value.json())
+            .then(value => {
+                setData(value)
+            })
+
+    }, []);
+return data
+}
+
+export {useFetch}
