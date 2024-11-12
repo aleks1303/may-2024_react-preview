31bddf7c6592de3cc6fef660542a1d4310b37919
aleks1303 <aleksbulda13@gmail.com>
2024-11-12 20:26:04 +0200

    react_preview_useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
+// useEffect - дозволяє зробити щось N кількість разів
+// це буде один раз якщо в deps буде пустий масив (масив залежностей)
+// або якщо передати в масив залежностей посилання на якийсь об'єкт який буде змінювати свій стан
+// тобі useEffect буде спрацьовувати N кількість раз
+
+// в середині useEffect може бути callback function
+// ця функція відпрацьовує тоді коли відпрацьовує useEffect
+
+const App: FC = () => {
+  const [x, setX] = useState(0)
+  useEffect(() => {
+    console.log('work')
+
+    return () =>{
+      console.log('!!!')
+    }
+  }, [x]);
+
   return (
     <div>
-
+        <button onClick={()=>{
+          setX(prevState => ++prevState)
+        }}>click</button>
     </div>
   );
 }
