73890ca051c349185d933b2abf633e52618b21cc
aleks1303 <aleksbulda13@gmail.com>
2024-11-14 14:23:34 +0200

    react_preview_component_use-case(7)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {FC} from 'react';
 import './App.css';
+import UserComponent from "./components/UserComponent";
 
-function App() {
+// use_case - це робити компоненти які будуть робити запит до баз даних,
+// наприклад, як jsonplaceholder
+
+// hoc - hi order component
+// такі роду компоненти мають назву зазвичай з with, тобто додаємо (з)
+
+
+const App:FC = () => {
   return (
     <div>
-
+        <UserComponent/>
     </div>
   );
 }
