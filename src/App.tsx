bb4339073bfacddeec4f744fd30f5c929e3b6a4e
aleks1303 <aleksbulda13@gmail.com>
2024-11-18 22:46:07 +0200

    react_preview_state_manager_redux_toolkit_async_Thunk (9)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import './App.css';
+import {postActions, useAppDispatch, useAppSelector, userActions} from "./redux/store";
 
 const App:FC = () => {
+
+  const {userSlice:{users}, postSlice:{posts}} = useAppSelector(state => state);
+
+  const dispatch = useAppDispatch();
+  
+
+  useEffect(() => {
+    dispatch(userActions.loadUsers())
+    dispatch(postActions.loadPosts())
+  }, []);
   return (
     <div>
-
-
+      {
+        users.map(user => <div key={user.id}>{user.name} : {user.email}</div>)
+      }
+      <hr/>
+      {
+        posts.map(post => <div key={post.id}>{post.title}</div>)
+      }
 
     </div>
   );
